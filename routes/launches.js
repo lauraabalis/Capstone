01d15c188cd2a8464063183d3a8c2b0edc4026d8
Laura Balis <lauraab1@cox.net>
2017-07-19 09:46:26 -0700

    Fifth commit

--- routes/launches.js
+++ routes/launches.js
@@ -2,41 +2,25 @@ var express = require('express');
 var router = express.Router();
 var knex = require('../db/knex');
 
-// router.get('/', function(req, res, next) {
-//   knex.raw('SELECT * FROM launch').then(function(collection) {
-//   var collection = [
-//     {date: "7/23/17", status: "LEAN CANVAS"},
-//     {date: "8/1/17", status: "CUSTOMER PROFILES"},
-//     {date: "9/1/17", status: "MVP MESSAGING"},
-//     {date: "9/15/17", status: "GRAPHICS"},
-//     {date: "9/17/17", status: "SOCIAL ACCOUNTS"},
-//     {date: "9/24/17", status: "IDENTIFY INFLUENCERS"},
-//     {date: "9/30/17", status: "INFLUENCER MESSAGING & COMP"},
-//     {date: "10/1/17", status: "TEASER ARTICLE"},
-//     {date: "10/15/17", status: "SIZZLE"},
-//     {date: "10/31/17", status: "LANDING PAGE"}
-//   ];
-//     res.render('launches', {
-//       title: 'Launch Plan Title',
-//       collection: collection
-//     });
-//   });
-// });
-
-module.exports = router;
-
 router.get('/', function(req, res, next) {
   var collection = [
-    {date: "7/23/17", status: "LEAN CANVAS"},
-    {date: "8/1/17", status: "CUSTOMER PROFILES"},
-    {date: "9/1/17", status: "MVP MESSAGING"},
-    {date: "9/15/17", status: "GRAPHICS"},
-    {date: "9/17/17", status: "SOCIAL ACCOUNTS"},
-    {date: "9/24/17", status: "IDENTIFY INFLUENCERS"},
-    {date: "9/30/17", status: "INFLUENCER MESSAGING & COMP"},
-    {date: "10/1/17", status: "TEASER ARTICLE"},
-    {date: "10/15/17", status: "SIZZLE"},
-    {date: "10/31/17", status: "LANDING PAGE"}
+    // {date: "7/23/17", status: "LEAN CANVAS"},
+    // {date: "8/1/17", status: "CUSTOMER PROFILES"},
+    // {date: "9/1/17", status: "MVP MESSAGING"},
+    // {date: "9/15/17", status: "GRAPHICS"},
+    // {date: "9/17/17", status: "SOCIAL ACCOUNTS"},
+    // {date: "9/24/17", status: "IDENTIFY INFLUENCERS"},
+    // {date: "9/30/17", status: "INFLUENCER MESSAGING & COMP"},
+    // {date: "10/1/17", status: "TEASER ARTICLE"},
+    // {date: "10/15/17", status: "SIZZLE"},
+    // {date: "10/31/17", status: "LANDING PAGE"}
   ];
-  res.render('launches', { title: 'Launch Plan Title', collection: collection });
+  knex.raw('SELECT * FROM launch').then(function(stuff) {
+    res.render('launches', {
+      title: 'Launch Plan Title',
+      collection: stuff.rows
+    });
+  });
 });
+
+module.exports = router;
